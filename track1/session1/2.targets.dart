907647312b30583a20739665a5740d15f02a3f87
Mohamed Abouzaid <119829381+mo-adel-tronic@users.noreply.github.com>
2024-11-21 10:43:07 +0200

    add session 1

---
+++ track1/session1/2.targets.dart
@@ -0,0 +1,35 @@
+/*
+! Basics In Dart
+
+? Editor & Dart Ext
+? Entry Point.
+? Print, user_input.
+? Variables & Constants
+? Data Types
+? Operators
+? Conditions
+? Loops
+? Function
+? Regular Expression
+? Error Handling
+? snippets & extension
+*/ 
+
+/*
+? Editor & Dart Ext
+
+* Error Lens
+* Better Comments
+* bloc
+* Bracket Pair Colorization Toggler
+* dart
+* Firebase Explorer
+* flutter
+* Image preview
+* Material Icon Theme
+* Prettier - Code formatter
+* Pubspec Assist
+* dart-import
+* Clean Flutter Architecture
+* Code Runner
+*/
